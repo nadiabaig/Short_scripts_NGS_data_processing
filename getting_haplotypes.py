8bb994b5a5992d8acef82344aa1095204bf6a739
Nadia Baig <51199820+nadiabaig@users.noreply.github.com>
2023-08-16 13:42:21 +0200

    Update getting_haplotypes.py

--- getting_haplotypes.py
+++ getting_haplotypes.py
@@ -25,3 +25,35 @@ if __name__ == "__main__":
 
     with open("dummy_phased.vcf", "w") as vcf_file:
         vcf_file.writelines(vcf_lines)
+
+def get_haplotypes_from_vcf(vcf_filename):
+    haplotypes = {}
+
+    with open(vcf_filename, "r") as vcf_file:
+        for line in vcf_file:
+            if line.startswith("#"):
+                continue  # Skip header lines
+
+            fields = line.strip().split("\t")
+            chrom, pos, var_id, ref, alt, qual, filt, info, fmt = fields[:9]
+            samples = fields[9:]
+
+            for sample_index, sample in enumerate(samples):
+                sample_name = f"Sample{sample_index + 1}"
+                genotype = sample.split(":")[0]
+
+                if "|" in genotype:
+                    haplotypes.setdefault(sample_name, []).append(genotype.split("|"))
+                elif "/" in genotype:
+                    haplotypes.setdefault(sample_name, []).append(genotype.split("/"))
+
+    return haplotypes
+
+if __name__ == "__main__":
+    vcf_filename = "dummy_phased.vcf"
+    haplotypes = get_haplotypes_from_vcf(vcf_filename)
+
+    for sample, haplotype_list in haplotypes.items():
+        print(f"Haplotypes for {sample}:")
+        for index, haplotype in enumerate(haplotype_list, start=1):
+            print(f"Haplotype {index}: {' '.join(haplotype)}")
