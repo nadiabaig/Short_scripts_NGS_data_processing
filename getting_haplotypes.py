292cd49dff82c22fa2e2821980f6d4ac3d9be874
Nadia Baig <51199820+nadiabaig@users.noreply.github.com>
2023-08-16 14:46:03 +0200

    Update getting_haplotypes.py

--- getting_haplotypes.py
+++ getting_haplotypes.py
@@ -1,1 +1,7 @@
+ #CHROM  POS REF ALT 001 002 S003 004 005  \
+ 4   56   A   G       AGAG       AGAG       AGAG       AGAG       AGAG   
+ 4   78  GG  AT       TTGG          .          .          .          .   
+ 4  167   A   T       ATAT       TATA       ATAT       ATAT       ATAT
+
+
 
